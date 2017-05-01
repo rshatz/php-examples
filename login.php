d8ece1518cba37d49ce22cf20027abd5c02f886b
rshatz <richardshatz@outlook.com>
2017-04-30 22:06:58 -0700

    basic html form

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ritchie
+ * Date: 4/27/2017
+ * Time: 7:16 PM
+ */
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ritchie
+ * Date: 4/27/2017
+ * Time: 9:27 PM
+ */
