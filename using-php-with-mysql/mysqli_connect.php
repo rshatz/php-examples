aa689c129e0586b80fc68ac037f68909ca76f70b
unknown <richardshatz@outlook.com>
2017-06-11 20:47:32 -0700

    connecting to database

---
+++ using-php-with-mysql/mysqli_connect.php
@@ -0,0 +1,19 @@
+<?php #script 9.2 - mysqli_connect.php
+
+// This file contains the database access information.
+// This file also establishes a connection to MySQL
+// selects the database, and sets the encoding. 
+
+DEFINE ('DB_USER', 'username');
+DEFINE ('DB_PASSWORD', 'password');
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_NAME', 'sitename');
+
+// Make the connection:
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+    OR die('could not connect to MySQL: ' . mysqli_connect_error());
+
+//set the encoding
+mysqli_set_charset($dbc, 'utf8');
+
+    
