d69648ece990d2b98306a8f99a45f4d26a05a3d8
rshatz <richardshatz@outlook.com>
2017-06-15 18:37:04 -0700

    executing queries

--- using-php-with-mysql/register.php
+++ using-php-with-mysql/register.php
@@ -6,7 +6,6 @@
 
     // Check for form submission:
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
         // Initialize an error array
         $errors = array();
 
@@ -49,7 +48,7 @@
 
             // Make the query:
             $q = "INSERT INTO users (first_name, last_name, email, pass, registration_date) 
-                VALUES ('$fn', '$ln', '$e', SHA1('$p', NOW() ) ";
+                VALUES ('$fn', '$ln', '$e', SHA1('$p'), NOW() ) ";
 
             // Run the query.
             $r = @mysqli_query ($dbc, $q);
@@ -66,17 +65,33 @@
                 // DEBUGGING MESSAGE
                 echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' , $q . '</p>';
             }
-        }
-    }
 
+            // Close the database
+            mysqli_close($dbc);
+
+            // Include the footer and quit the script:
+            include ('includes/footer.html');
+            exit();
+        
+        } else { // Report the errors
+            echo '<h1>Error!</h1><p class="error">The following error(s) occurred:<br />';
+            // Print each error
+            foreach ($errors as $msg) {
+                echo " - $msg<br />\n";
+            }
+            echo '</p><p>Please try again.</p><p><br /></p>';
+        } // End of if (empty($errors))
+    } // End of main Submit conditional
 ?>
 
 <h1>Register</h1>
 <form action="register.php" method="post">
-    <p>First Name: <input type="text" name="first_name" size="15" maxlength="20"
-        value="<?php if(isset($_POST['first_name'])) echo $_POST['first_name']; ?>" /></p>
-    
-    <p><input type="submit" name="submit" value="Register" /></p>
+    <p>First Name: <input type="text" name="first_name" size="15" maxlength="20" value="<?php if (isset($_POST['first_name'])) echo $_POST['first_name']; ?>" /></p>
+	<p>Last Name: <input type="text" name="last_name" size="15" maxlength="40" value="<?php if (isset($_POST['last_name'])) echo $_POST['last_name']; ?>" /></p>
+	<p>Email Address: <input type="text" name="email" size="20" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>"  /> </p>
+	<p>Password: <input type="password" name="pass1" size="10" maxlength="20" value="<?php if (isset($_POST['pass1'])) echo $_POST['pass1']; ?>"  /></p>
+	<p>Confirm Password: <input type="password" name="pass2" size="10" maxlength="20" value="<?php if (isset($_POST['pass2'])) echo $_POST['pass2']; ?>"  /></p>
+	<p><input type="submit" name="submit" value="Register" /></p>
 </form>
 
 <?php include ('includes/footer.html'); ?>
